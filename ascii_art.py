19f0ec9ec01b9a3aa084b4d44893f7d2898bfe97
Ceci <cecilia.berriz@gmail.com>
2025-05-12 02:43:58 -0700

    add ascii image converter

---
+++ ascii_art.py
@@ -0,0 +1,46 @@
+from PIL import Image
+
+
+ASCII_CHARS = "@@##%%\"|()[]{}*-=-<>!~^=-:`'._ -:. .,  "
+
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    aspect_ratio = height / width
+    new_height = int(new_width * aspect_ratio * 0.5)
+    return image.resize((new_width, new_height))
+
+
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    ascii_str = ''
+    for pixel in pixels:
+        ascii_str += ASCII_CHARS[pixel % len(ASCII_CHARS)]
+    return ascii_str
+
+
+def main():
+    path = input('Enter a path to an image file: ')
+    if not path:
+        path = './katara.png'
+    try:
+        image = Image.open(path)
+    except Exception as e:
+        print(f"Unable to open image: {e}")
+        return
+
+    # process image
+    image = resize_image(image)
+    image = image.convert('L')  # grayscale
+
+    # convert to ASCII
+    ascii_str = pixels_to_ascii(image)
+    img_width = image.width
+    ascii_img = '\n'
+    for i in range(0, len(ascii_str), img_width):
+        ascii_img += ascii_str[i:i+img_width] + '\n'
+    print(ascii_img)
+
+
+if __name__ == '__main__':
+    main()
